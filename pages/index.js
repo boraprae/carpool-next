c6f42e19ac3c56413f53bbc6cfc03deb71cd2540
Thanchanok T <6231302021@lamduan.mfu.ac.th>
2022-11-02 13:39:14 +0700

    SSr

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
-
-export default function Home() {
+export default function Home({posts}) {
   return (
-    <div>
-      
+    <div> 
+        <h1>MFU Carpool</h1>
+        {
+          posts.map((post) => {
+            return (
+              <div key={post.postID}>
+                <p>{post.title}</p>
+                <p>{post.price} baht</p>
+              </div>
+            )
+          })
+        }
     </div>
   )
 }
+
+// SSR, real-time
+export async function getServerSideProps() {
+  try {
+    const res = await fetch('http://localhost:9000/post');    
+    if(res.ok) {
+      const posts = await res.json();
+      return { props: {posts}};
+    }
+    throw Error('server response not ok');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('error');
+  }
+}
+
